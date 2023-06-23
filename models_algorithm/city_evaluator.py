9bdeea775aac92fc5d53600d00ca4e5e69a3793a
Gavin Fisher <gdf73278@iastate.edu>
2023-06-23 10:29:37 -0500

    city_evaluator can read in a parent folder, iterate through each folder calling house_evaluator. House_evaluator can read all of the images in each individual child folder it recieves and run through models (only printing for now)

--- models_algorithm/city_evaluator.py
+++ models_algorithm/city_evaluator.py
@@ -4,10 +4,15 @@
 # calls house_evaluator to evaluate with AI models
 # prints to csv file
 
+import os
+import subprocess
 
+main_folder = 'house_image_inputs_test'
 
-
-
+for folder_name in os.listdir(main_folder):
+    if os.path.isdir(os.path.join(main_folder, folder_name)):
+        address_folder_path = os.path.join(main_folder, folder_name)
+        subprocess.call(["python", "house_evaluator.py", address_folder_path])
 
 
 
